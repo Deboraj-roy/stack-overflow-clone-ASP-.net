fdc50e185dd8403df7993dc1179493c8074f571c
deboraj-roy <deborajroy123@gmail.com>
2024-04-13 11:25:19 +0600

    Program cs configure

---
+++ src/Stackoverflow-clone/Stackoverflow.API/ApiModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace Stackoverflow.API
+{
+    public class ApiModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
--- src/Stackoverflow-clone/Stackoverflow.API/Program.cs
+++ src/Stackoverflow-clone/Stackoverflow.API/Program.cs
@@ -1,8 +1,13 @@
-
-
-
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
 using Serilog;
 using Serilog.Events;
+using Stackoverflow.API;
+using Stackoverflow.Application;
+using Stackoverflow.Infrastructure;
+using System.Reflection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
@@ -22,14 +27,49 @@ try
             .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
             .Enrich.FromLogContext()
             .ReadFrom.Configuration(builder.Configuration));
+
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    var migrationAssembly = Assembly.GetExecutingAssembly().FullName;
+
+    builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+    builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+    {
+        containerBuilder.RegisterModule(new ApplicationModule());
+        containerBuilder.RegisterModule(new InfrastructureModule(connectionString,
+            migrationAssembly));
+        containerBuilder.RegisterModule(new ApiModule());
+    });
+
     // Add services to the container.
 
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    //builder.Services.AddIdentity();
+    //builder.Services.AddJwtAuthentication(builder.Configuration["Jwt:Key"], builder.Configuration["Jwt:Issuer"],
+    //    builder.Configuration["Jwt:Audience"]);
+
+    //builder.Services.AddAuthorization(options =>
+    //{
+    //    options.AddPolicy("CourseViewRequirementPolicy", policy =>
+    //    {
+    //        policy.AuthenticationSchemes.Clear();
+    //        policy.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
+    //        policy.RequireAuthenticatedUser();
+    //        policy.Requirements.Add(new CourseViewRequirement());
+    //    });
+    //});
+
+
+    //builder.Services.AddSingleton<IAuthorizationHandler, CourseViewRequirementHandler>();
+
     builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+    builder.Services.AddMediatR(cfg => { cfg.RegisterServicesFromAssembly(typeof(Program).Assembly); });
 
     var app = builder.Build();
+
+    Log.Information("Application Starting...");
 
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
