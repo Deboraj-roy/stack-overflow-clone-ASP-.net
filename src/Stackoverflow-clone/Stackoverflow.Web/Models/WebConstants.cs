8894247167b3e12cdd78c4103cae825b1b290850
“DEBORAJ <deborajroy123@gmail.com>
2024-04-26 11:13:19 +0600

    Mapping Port For Docker

--- src/Stackoverflow-clone/Stackoverflow.Web/Models/WebConstants.cs
+++ src/Stackoverflow-clone/Stackoverflow.Web/Models/WebConstants.cs
@@ -1,10 +1,12 @@
-﻿namespace Stackoverflow.Web.Models
+﻿﻿using System;
+namespace Stackoverflow.Web.Models
 {
     public static class WebConstants
     {
         //check your API where it runs then store here
         //public const string ApiUrl = "http://localhost:5293/v3/";
-        public const string ApiUrl = "https://localhost:7278/v3/";
+        //public const string ApiUrl = "https://localhost:7278/v3/";
+        public static string ApiUrl = System.Environment.GetEnvironmentVariable("API_URL") ?? "http://localhost:5293/v3/";
     }
 
 }
