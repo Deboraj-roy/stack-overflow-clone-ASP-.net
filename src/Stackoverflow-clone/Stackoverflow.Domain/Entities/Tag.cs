26fbb8566387c9a5301670501080ba7f488f21c6
“DEBORAJ <deborajroy123@gmail.com>
2024-05-04 23:55:08 +0600

    class

---
+++ src/Stackoverflow-clone/Stackoverflow.Domain/Entities/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stackoverflow.Domain.Entities
+{
+    public class Tag : IEntity<Guid>
+    {
+        public Guid Id { get; set; } // unique identifier
+        public string Name { get; set; }
+    }
+}
---
+++ src/Stackoverflow-clone/Stackoverflow.Domain/Entities/UserBadge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stackoverflow.Domain.Entities
+{
+    public class UserBadge : IEntity<Guid>
+    {
+        public Guid Id { get; set; } // unique identifier
+        public Guid UserId { get; set; } // foreign key referencing the User who earned the badge
+        public string BadgeName { get; set; }
+        public string BadgeDescription { get; set; }
+        public DateTime DateEarned { get; set; }
+    }
+}
